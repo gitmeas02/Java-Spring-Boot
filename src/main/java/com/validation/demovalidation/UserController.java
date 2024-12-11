3d4ffb700c2b28a574083d0dd87bf01f762483cc
gitmeas02 <measreaksmey02@gmail.com>
2024-12-11 07:31:59 +0700

    Validation Spring boot + Thymleaf

--- src/main/java/com/validation/demovalidation/UserController.java
+++ src/main/java/com/validation/demovalidation/UserController.java
@@ -3,6 +3,7 @@ package com.validation.demovalidation;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 // import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.validation.Valid;
 
 
 @Controller
@@ -20,14 +23,28 @@ public class UserController {
 
     @GetMapping("/create")
     public String showCreatePage(Model model) {
-        model.addAttribute("users_info", new UserEntity());
+        model.addAttribute("users_info", new UserDTO());
         model.addAttribute("titleHeader", "Create Page");
         model.addAttribute("contents", "fragments/create");  // Pass the fragment name for the create page
         return "contents";  // Render the contents.html where create fragment is included
     }
 
     @PostMapping("/save")
-    public String handleCreate(@ModelAttribute UserEntity userEntity) {
+    public String handleCreate(@Valid @ModelAttribute("users_info") UserDTO userDTO,
+    BindingResult bindingResult, Model model
+    ) {
+
+if(bindingResult.hasErrors()){
+    model.addAttribute("titleHeader", "Create Page");
+    model.addAttribute("contents", "fragments/create");
+    return "contents";
+}
+ UserEntity userEntity= new UserEntity(
+userDTO.getFirstName(),
+userDTO.getLastName(),
+userDTO.getEmail(),
+userDTO.getPrice()
+);
         userRepository.save(userEntity);  // Save to database
         return "redirect:/list_form";  // Redirect to the list page after creation
     }
@@ -48,7 +65,17 @@ public class UserController {
     }
     @GetMapping("/edit/{id}")
     public String EditUser(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("users_info", userRepository.findById(id));
+        UserEntity userEntity = userRepository.findById(id).orElse(null);
+        if(userEntity != null){
+            UserDTO userDTO = new UserDTO();
+            userDTO.setFirstName(userEntity.getFirstName());
+            userDTO.setLastName(userEntity.getLastName());
+            userDTO.setEmail(userEntity.getEmail());
+            userDTO.setPrice(userEntity.getPrice());
+
+            model.addAttribute("users_info", userDTO);
+        }
+        // model.addAttribute("users_info", userRepository.findById(id));
         model.addAttribute("contents", "fragments/create");
         model.addAttribute("titleHeader", "Edit");
         return "contents";
--- src/main/java/com/validation/demovalidation/UserDTO.java
+++ src/main/java/com/validation/demovalidation/UserDTO.java
@@ -1,5 +1,55 @@
 package com.validation.demovalidation;
 
+
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+
 public class UserDTO {
-    
+   @NotBlank(message = "Must input your  First Name")
+   @Size(min=2,max=50,message ="First Name must be between 2 and 50 characters" )
+    private String firstName;
+
+   @NotBlank(message = "Must input your Last Name")
+   @Size(min=2,max=50,message="Last Name must be between 2 and 50 characters")
+    private String lastName;
+
+
+    @Email(message = "Please input email")
+    @NotEmpty(message = "Email required")
+    private String email;
+
+      @DecimalMin(value = "20.0", inclusive = false, message = "Price must be greater than 20")
+      @DecimalMax(value = "10000.0", inclusive = true, message = "Price must not exceed 10000")
+       private Double price;
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Double getPrice() {
+        return price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
